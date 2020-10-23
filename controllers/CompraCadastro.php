34391dea83f3fc0f60cede57aa6f116ca8f68fc7
rpererah <rpererah@gmail.com>
2020-10-23 15:10:42 -0300

    logica compra

---
+++ controllers/CompraCadastro.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'models/Compra.php';
+$Compra= new Compra();
+if(isset($_POST['submit'])){
+	$id_usuario = $_POST['id_usuario'];
+	$id_produto=$_POST['id_produto'];
+    $quantidade = $_POST['quantidade'];
+   
+	$Compra->setId_usuario($id_usuario);
+	$Compra->setId_produto($id_produto);
+    $Compra->setQuantidade($quantidade);
+	$Compra->insert($id_usuario,$id_produto,$quantidade);
+}
+?>
